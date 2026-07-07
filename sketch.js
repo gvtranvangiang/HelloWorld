80beefff76482f5f903e69047e3d38e87efd8d8e
Tran Van Giang <tranvangiang@gmail.com>
2026-07-07 22:11:08 +0700

    Add mousePressed function

--- sketch.js
+++ sketch.js
@@ -2,7 +2,14 @@ function setup() {
   createCanvas(500, 500);
 }
 
-function draw() {
-  background(220);
-  text("Hello World!", 150, 200);
+function draw() {  
 }
+mousePressed = function() {
+  background(220);
+  fill(255, 0, 0);
+  text("Hello World 1!", 150, 200);
+  
+  fill(random(255), random(255), random(255));
+  rect(random(width), random(height), random(50, 100), random(50, 100));
+}
+
