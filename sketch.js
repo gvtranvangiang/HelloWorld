d62d9bc8c2927c25f19903722aee36ebd5acdad1
Tran Van Giang <tranvangiang@gmail.com>
2026-07-08 11:30:05 +0700

    Add Hello World 1

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ mousePressed = function() {
   background(220);
   fill(255, 0, 0);
   text("Hello World 1!", 150, 200);
+  text("Hello World 1 from tranvangiang!", 150, 200);
   
   fill(random(255), random(255), random(255));
   rect(random(width), random(height), random(50, 100), random(50, 100));
